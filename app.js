e7358b09e75b006d4d11a0e28caafc1e686de44c
jiwenxing <jiwenxing@jd.com>
2017-03-07 19:04:52 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var resize = require('./lib/resize'),
+cfg = require('./config'),
+express = require('express'),
+port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000,
+host = process.env.OPENSHIFT_NODEJS_IP;
+
+var photosPath = './resources/photos';
+resize.init(photosPath)
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets/dist/'))
+
+app.use('/', require('./lib/gallery.js')(Object.assign({
+  staticFiles : 'resources/photos',
+  urlRoot : '/',
+  title : 'Zing Gallery',
+  render : false
+}, cfg)), function(req, res, next){
+  return res.render('gallery', Object.assign({ 
+  	galleryHtml : req.html
+  }, cfg));
+});
+
+
+app.listen(port, host);
+host = host || 'localhost';
+console.log('node-gallery listening on ' + host  + ':' + port);
