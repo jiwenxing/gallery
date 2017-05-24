e850e5c9f731149ef268240ac635fe2e18040610
Jverson <blite1228@163.com>
2017-05-25 00:41:47 +0800

    add des

--- config.js
+++ config.js
@@ -4,21 +4,22 @@ module.exports = {
 		noAccess: '抱歉，你没有权限访问'
 	},
 	albums: {
-		/*"相册1": {},
-        "相册2": {},*/
-        /*"Macau": {
+        "Macau": {
+        	description : "东方拉斯维加斯",
         	thumbnail : "20160918_114817.jpg"
-        },*/
+        },
 		"银川": {
+		  description : "传说中的西部小江南",
 		  thumbnail : "DSCF1677.JPG"
 		  // sort: 1
 		},
 		"乌镇": {
+		  description : "如果时空交错，也许我们就能在此相遇",
 		  thumbnail : "DSC_0099.jpg"
 		  // sort: 1
 		},
 		"Quebec Canada": {
-		  // description : "没有什么能够阻挡",
+		  description : "一次学术的朝圣之旅",
 		  thumbnail : "DSCN0826.JPG",
 		  name: "Quebec Canada"
 		}
